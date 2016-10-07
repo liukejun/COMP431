dff432ab7bf5a5f4ea095313bc6936582151eb4f
liukejun <liukejun5@hotmail.com>
2016-10-06 23:13:02 -0500

    Add files via upload

--- inclass-14/e2e/login.spec.js
+++ inclass-14/e2e/login.spec.js
@@ -18,10 +18,7 @@ describe('Test Dummy Server Example Page', () => {
             })
             .then(done))
     })
-    var oldMsg = ''
-    var oldheadline = ''
-    const pre = 'you are logged in as kl50 "'
-    const post ='"'
+    
     it("Update the headline and verify the change", (done) => {
         // IMPLEMENT ME
         // find the headline input
@@ -29,24 +26,8 @@ describe('Test Dummy Server Example Page', () => {
         // verify the headline is updated
         // .sendKeys(the old headline message)
         // verify the headline is updated
-
-        // sleep(500)
-        // .then(findId('newHeadline').sendKeys('test me'))
-        // .then(findId('headline').click())
-        // .then(sleep(1000))
-        // .then(findId('message').getText()
-        //     .then(text => {
-        //             expect(text).to.equal('Update headline to "test me"')
-        //         })
-        //     .then(done))
-        findId('message').getText()
-        .then(text => {
-                // 
-                console.log(text)
-            })
-        .then(oldheadline = oldMsg.substring(pre.length))
-        .then(console.log(oldheadline))
-        .then(sleep(500))
+        
+        sleep(500)
         .then(findId('newHeadline').sendKeys('I am a new headline'))
         .then(findId('headline').click())
         .then(sleep(1000))
